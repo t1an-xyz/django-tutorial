4b24f0984decdeffe55a33bf16ac14b107d35f76
Theo <62950302+t1an-xyz@users.noreply.github.com>
2024-06-25 14:35:02 -0400

    Update view for customer

--- accounts/views.py
+++ accounts/views.py
@@ -22,5 +22,9 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    
+    context = {'customer': customer, 'orders': orders, 'num_orders': orders.count()}
+    return render(request, 'accounts/customer.html', context)
